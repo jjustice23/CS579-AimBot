130ca80ce255c85b6371c092e50ae35c214381bf
Jacob <justice3@wisc.edu>
2025-03-22 15:12:47 -0500

    project start

---
+++ Assets/grab.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class GrabObject : MonoBehaviour
+{
+    private XRGrabInteractable grabInteractable;
+
+    private void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // Subscribe to the selectEntered and selectExited events directly
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        // Get the hand/controller transform when grabbed
+        Transform handTransform = args.interactorObject.transform;
+
+        // Set the object’s position and rotation to match the hand
+        transform.position = handTransform.position;
+        transform.rotation = handTransform.rotation;
+
+        // Optionally, set isKinematic on the Rigidbody to ensure it doesn't get affected by physics while grabbed
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        // Reset isKinematic on the Rigidbody when released
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void Update()
+    {
+        // While grabbed, make sure the object follows the hand's position and rotation
+        if (grabInteractable.isSelected)
+        {
+            Transform handTransform = grabInteractable.interactorsSelecting[0].transform;
+            transform.position = handTransform.position;
+            transform.rotation = handTransform.rotation;
+        }
+    }
+}
