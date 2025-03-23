3672c3c68cede14a89f6cdf647f0b828cd3354d6
Jacob <justice3@wisc.edu>
2025-03-23 10:00:21 -0500

    corrected target spawn and modified main room

--- Assets/SphereHit.cs
+++ Assets/SphereHit.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class SphereHit : MonoBehaviour
 {
+    public GameObject floor;
+    public GameObject wall;
+    public GameObject room;
     public void OnHit()
     {
+        /* condtions: zero needs to be the centerpoint of room, sphereRadius should not be larger than half any room dimension, 
+                      and the floor should be leveled so the top is at y=0
+
+           other notes: x=0 to the positive direction is considered the "target area" and anything to the negative direction is the "player area"
+                        
+        */
+
+        // if these are met, the room should be scalable (potentially to modify difficulty), but needs to be tested
+
+        float sphereRadius = transform.localScale.x / 2;  // seems correct in in concept, debug at some point
+        float floorLen = floor.transform.localScale.x * room.transform.localScale.x;
+        float floorWidth = floor.transform.localScale.z * room.transform.localScale.z;
+        float wallHeight = wall.transform.localScale.y * room.transform.localScale.y;
         // todo: clean up ranges
         Debug.Log("test");
-        Instantiate(gameObject, new Vector3(Random.Range(0,10), Random.Range(0,5), Random.Range(-5,5)), Quaternion.identity);
+
+        float x = Random.Range(0 + sphereRadius, (floorLen / 2) - sphereRadius);
+        float y = Random.Range(0 + sphereRadius, wallHeight - sphereRadius);
+        float z = Random.Range((-1 * (floorWidth / 2)) + sphereRadius, (floorWidth / 2) - sphereRadius);
+        Instantiate(gameObject, new Vector3(x, y, z), Quaternion.identity);
+
         Destroy(gameObject);
     }
 }
